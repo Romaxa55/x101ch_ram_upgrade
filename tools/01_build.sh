e8cd49048ee84f93227316cdecd1e5e96d70442d
Roman Shamagin <romaxa55@bk.ru>
2021-11-07 00:24:04 +0300

    Update 01_build.sh

--- tools/01_build.sh
+++ tools/01_build.sh
@@ -4,4 +4,4 @@ if [ -d "build" ]; then rm -rf build; fi
 
 mkdir build && cd build
 cmake .. && make && cp spd-tool ../.. && cd .. && rm -rf build && chmod +x ../spd-tool
-sudo gcc -o ../ddr3spd ddr3spd.c
+gcc -o ../ddr3spd ddr3spd.c
