8b66805b20faae14a82ee1329038a4e183024b8a
Roman Shamagin <romaxa55@bk.ru>
2021-11-06 23:18:54 +0300

    Update 01_build.sh

--- tools/01_build.sh
+++ tools/01_build.sh
@@ -4,6 +4,4 @@ if [ -d "build" ]; then rm -rf build; fi
 
 mkdir build && cd build
 cmake .. && make && cp spd-tool ../.. && cd .. && rm -rf build && chmod +x ../spd-tool
-
-../spd-tool -i ../BIOS/X101CH-ASUS-1203.ROM
-../spd-tool -i ../dump_spd_1931704
+sudo gcc -o ../ddr3spd ddr3spd.c
